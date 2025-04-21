4749bb62735a1b4c2e6ee4cb979d43270e6e69b6
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-04-21 10:19:40 +0200

    Update template.cpp

--- EX01/template.cpp
+++ EX01/template.cpp
@@ -17,19 +17,36 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  // Get the hostname of the processor
-  char hostname[MPI_MAX_PROCESSOR_NAME];
-  int hostname_len;
-  MPI_Get_processor_name(hostname, &hostname_len);
+  char *data = (char *)malloc(1024*1024);
+  if (!data) {
+    fprintf(stderr, "Failed to allocate buffer\n");
+    MPI_Finalize();
+    return EXIT_FAILURE;
+}
 
-  // Output the rank, size, hostname, and all CLI parameters
-  std::cout << "Hello from rank " << rank << " out of " << size << " processes on host " << hostname
-            << " with cli parameters: '";
-  for (int i = 1; i < argc; ++i) {
-    std::cout << argv[i];
-    if (i < argc - 1) std::cout << ", ";
+  for (int size = 1024; size <= 1024*1024; size *= 2){
+    double start_time = MPI_Wtime();
+
+    for (int i = 0; i < 10; i++){
+      if (rank == 0) {
+        MPI_Send(data, size, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(data, size, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      } else if (rank == 1) {
+        MPI_Recv(data, size, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(data, size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+      }
+    }
+
+    double end_time = MPI_Wtime();
+
+    if (rank == 0) {
+      double total_time = end_time - start_time;
+      double avg_round_trip = (total_time / 10) * 1000; // in ms
+      double half_trip = avg_round_trip / 2;
+      printf("Message size: %6d bytes | Full RTT: %.6f ms | Half RTT (Latency): %.6f ms\n",
+             size, avg_round_trip, half_trip);
+    }
   }
-  std::cout << "'" << std::endl;
 
   // Finalize MPI
   MPI_Finalize();
