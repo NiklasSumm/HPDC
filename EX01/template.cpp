0651dbedce6bc955b3776f1636ef07b42df1081c
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-05-03 16:14:11 +0200

    Update template.cpp

--- EX01/template.cpp
+++ EX01/template.cpp
@@ -13,43 +13,37 @@ int main(int argc, char *argv[]) {
   // Initialize MPI
   MPI_Init(&argc, &argv);
 
+  MPI_Status status;
+  int dummy = 1;
+
   // Get the rank and size of the MPI communicator
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  if (rank == 1){
-    double matrixA[2048][2048];
-    double matrixB[2048][2048];
-    double matrixC[2048][2048];
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(-10, 10);
-
-    for (int i = 0; i < 2048; i++){
-      for (int j = 0; j < 2048; j++){
-        matrixA[i][j] = dis(gen);
-        matrixB[i][j] = dis(gen);
+  double start_time = MPI_Wtime();
+  for (int iteration = 0; iteration < 10; iteration++){
+    if (rank == 0) {
+      // Root waits for notification from all other processes
+      for (int i = 1; i < size; i++) {
+          MPI_Recv(&dummy, 1, MPI_INT, i, 0, comm, &status);
       }
-    }
-
-    double start_time = MPI_Wtime();
-
-    for (int i = 0; i < 2048; i++){
-      for (int j = 0; j < 2048; j++){
-        double sum = 0;
-        for (int k = 0; k < 2048; k++){
-          sum += matrixA[k][j] * matrixB[i][k];
-        }
-        matrixC[i][j] += sum;
+      // Root sends release signal to all processes
+      for (int i = 1; i < size; i++) {
+          MPI_Send(&dummy, 1, MPI_INT, i, 0, comm);
       }
+    } else {
+      // Notify root
+      MPI_Send(&dummy, 1, MPI_INT, 0, 0, comm);
+      // Wait for release signal
+      MPI_Recv(&dummy, 1, MPI_INT, 0, 0, comm, &status);
     }
+  }
+  double end_time = MPI_Wtime();
 
-    double end_time = MPI_Wtime();
-    double total_time = end_time - start_time;
-
-    printf("Matrix multiply needed %f ms", 1000*(total_time));
+  if (rank == 0) {
+    double total_time = (end_time - start_time) * 1000;
+    printf("%i Processes - Total time: %f ms\n", size, total_time);
   }
 
   // Finalize MPI
