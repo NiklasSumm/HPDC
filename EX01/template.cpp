be4d3e5045e5efb2dfae628591a0915cad5d0794
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-05-06 13:42:23 +0200

    Update template.cpp

--- EX01/template.cpp
+++ EX01/template.cpp
@@ -23,7 +23,21 @@ int main(int argc, char *argv[]) {
 
   double start_time = MPI_Wtime();
   for (int iteration = 0; iteration < 10; iteration++){
-    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank == 0) {
+      // Root waits for notification from all other processes
+      for (int i = 1; i < size; i++) {
+          MPI_Recv(&dummy, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      }
+      // Root sends release signal to all processes
+      for (int i = 1; i < size; i++) {
+          MPI_Send(&dummy, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+      }
+    } else {
+      // Notify root
+      MPI_Send(&dummy, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      // Wait for release signal
+      MPI_Recv(&dummy, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    }
   }
   double end_time = MPI_Wtime();
 
