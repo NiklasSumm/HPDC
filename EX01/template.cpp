cd7fd480cc7599d28cab8772deb00f9b8884bdb0
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-05-19 17:25:24 +0200

    Update template.cpp

--- EX01/template.cpp
+++ EX01/template.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int N = 5;
+    int N = 2048;
 
     // Argumente parsen
     for (int i = 1; i < argc; ++i) {
